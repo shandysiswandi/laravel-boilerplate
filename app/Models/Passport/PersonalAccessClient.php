52b8d975798c4299bbe812047aede1428e9bf4ec
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-08 21:55:33 +0700

    create model passport

---
+++ app/Models/Passport/PersonalAccessClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Passport;
+
+use Laravel\Passport\PersonalAccessClient as PassportModel;
+
+class PersonalAccessClient extends PassportModel
+{
+    //
+}
---
+++ app/Models/Passport/Token.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Passport;
+
+use Laravel\Passport\Token as PassportClient;
+
+class Token extends PassportClient
+{
+    //
+}
